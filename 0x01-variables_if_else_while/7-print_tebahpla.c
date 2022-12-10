087af78824295bff5443e75d7b243c5706fceb0d
kaleab <kale27adn@gmail.com>
2022-12-10 08:43:10 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Entry
+ * Description: Alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
