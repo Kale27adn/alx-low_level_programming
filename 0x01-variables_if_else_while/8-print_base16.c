b875272b17c98f31f05c44f5aaec65b7a9a2f417
kaleab <kale27adn@gmail.com>
2022-12-10 09:06:52 -0800

    task9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Main - Entey
+ * Description: Alphabet in lowercase
+ *
+ * Retrun: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
