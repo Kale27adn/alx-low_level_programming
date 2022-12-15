c9a8f90f00aad61ca0feb48abd97bdf5f0575e1d
kaleab <kale27adn@gmail.com>
2022-12-15 14:02:28 -0800

    task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number
+ *
+ * Return: o
+ */
+
+void print_most_numbers(void)
+{
+int (number = 48; number < 58; number++)
+{
+if ((number == 50 || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
+}
