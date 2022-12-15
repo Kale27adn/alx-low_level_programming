aea5ae534aa617d30fa0ba35e513a7557d5e8365
kaleab <kale27adn@gmail.com>
2022-12-15 02:59:59 -0800

    check

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main -Entry
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int i;
+	{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zeron\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
+return (0);
+}
