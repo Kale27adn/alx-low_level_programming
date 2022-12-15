0f34eacf4d78c3ac9e95954567221169aabb5892
kaleab <kale27adn@gmail.com>
2022-12-15 00:21:23 -0800

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/***
+ * main - tests function that prints if imtrger is positive or negetive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positve_or_negetive(i);
+
+	return (0);
+}
